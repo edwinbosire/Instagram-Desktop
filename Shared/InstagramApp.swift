6af2903be410168262ccd8d26fbe7ee95a7dc358
Edwin Bosire <me@edwinb.co.uk>
2021-06-26 16:03:32 +0100

    Initial Commit

---
+++ Shared/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Shared
+//
+//  Created by Edwin Bosire on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
